cb0cde9e9924dae4dcfe8dc8efcedb8d830766ba
thaisartell <thaisartell@gmail.com>
2024-12-02 10:34:41 -0600

    Initial joystick header/main commit

---
+++ sarte016_joystick_library_v1.c
@@ -0,0 +1,84 @@
+#include "xc.h"
+#include "sarte016_joystick_library_v1.h"
+#include "troth004_utils.h"
+#include "troth004_lcd_library_v1.h"
+
+#define LCD_ADDRESS 0x78
+
+volatile int joystick_x = 0;
+volatile int joystick_y = 0;
+volatile int joystick_sel = 0;
+
+void init_ports(void){
+    // HOR -> AN0
+    AD1PCFGbits.PCFG0 = 0;
+    AD1CSSLbits.CSSL0 = 1;
+    
+    // VERT -> AN1
+    AD1PCFGbits.PCFG1 = 0;
+    AD1CSSLbits.CSSL1 = 1;
+    
+    // SEL -> AN2
+    AD1PCFGbits.PCFG2 = 0;
+    AD1CSSLbits.CSSL2 = 1;
+    
+}
+
+void init_adc(void){
+    //AD1CON1, 2, 3
+    AD1CON1 = 0;
+    AD1CON1bits.FORM = 0;
+    AD1CON1bits.SSRC = 0b111;
+    AD1CON1bits.ASAM = 0;
+    
+    AD1CON2 = 0;
+    
+    AD1CON3 = 0;
+    AD1CON3bits.SAMC = 0b11111;
+    AD1CON3bits.ADCS = 0;
+    
+    IFS0bits.AD1IF = 0;
+    IEC0bits.AD1IE = 1;
+}
+
+void init_joystick(void){
+    
+}
+
+void joystick_send_to_lcd(void){
+    
+}
+
+void pen_shift(void){
+    
+}
+
+//SEL -> AN2
+//Interrupt detects button clicks. Button click controls pen up/down
+void __attribute((interrupt, auto_psv)) _AD1Interrupt(void){
+    joystick_x = ADC1BUF0;
+    joystick_y = ADC1BUF1;
+    joystick_sel = ADC1BUF2;
+    IFS0bits.AD1IF = 0;
+}
+
+void pen_shift(void){
+    
+}
+
+int main(){
+    enable_I2C();
+    init_pic24();
+    init_joystick();
+    
+    
+    while (1) {
+        if (joystick_button_pressed){
+            pen_shift();
+        }
+        
+        joystick_send_to_lcd(void);
+    }
+    
+    return 1;
+}
---
+++ sarte016_joystick_library_v1.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   sarte016_joystick_library_v1.h
+ * Author: thai
+ *
+ * Created on November 28, 2024, 6:11 PM
+ */
+
+#ifndef SARTE016_JOYSTICK_LIBRARY_V1_H
+#define	SARTE016_JOYSTICK_LIBRARY_V1_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void init_pic24(void);
+    void init_joystick(void);
+    
+    
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SARTE016_JOYSTICK_LIBRARY_V1_H */
+
